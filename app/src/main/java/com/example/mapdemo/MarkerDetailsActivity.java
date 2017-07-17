3749641385d252bce72d94746f631c05dbbfa7e8
Maya Jeyendran <mayajey@fb.com>
2017-07-17 13:45:09 -0700

    photo file persists locally! yay

--- app/src/main/java/com/example/mapdemo/MarkerDetailsActivity.java
+++ app/src/main/java/com/example/mapdemo/MarkerDetailsActivity.java
@@ -1,5 +1,5 @@
-
 package com.example.mapdemo;
+
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -11,10 +11,11 @@ import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import java.io.File;
 
 import permissions.dispatcher.NeedsPermission;
@@ -23,18 +24,18 @@ import permissions.dispatcher.RuntimePermissions;
 @RuntimePermissions
 public class MarkerDetailsActivity extends AppCompatActivity {
 
-
     public final String APP_TAG = "SeenAds";
     public final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1034;
+    public final String ABSOLUTE_FILE_PATH = "/storage/emulated/0/Android/data/com.example.mapdemo/files/Pictures/SeenAds/";
     public String photoFileName = "photo.jpg";
 
     TextView tvTitle;
     TextView tvSnippet;
-    Button btnUploadPic;
+    ImageButton ibUploadPic;
     ImageView ivMarkerPhoto;
 
     // TODO make this prettier
-    // TODO pass image back in imageview
+    // TODO load the right image when clicked on details if picture has already been taken
     // TODO make this... work!
 
     @Override
@@ -44,11 +45,18 @@ public class MarkerDetailsActivity extends AppCompatActivity {
         // retrieve intent & setup
         String ID = getIntent().getStringExtra("title");
         String snippet = getIntent().getStringExtra("snippet");
+        photoFileName = photoFileName + ID;
         tvTitle = (TextView) findViewById(R.id.tvTitle);
         tvSnippet = (TextView) findViewById(R.id.tvSnippet);
         ivMarkerPhoto = (ImageView) findViewById(R.id.ivMarkerPhoto);
-        btnUploadPic = (Button) findViewById(R.id.btnUploadPic);
-        btnUploadPic.setOnClickListener(new View.OnClickListener() {
+        File imgFile = new  File(ABSOLUTE_FILE_PATH + photoFileName);
+        if(imgFile.exists()){
+            Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+            ImageView myImage = (ImageView) findViewById(R.id.ivMarkerPhoto);
+            myImage.setImageBitmap(myBitmap);
+        }
+        ibUploadPic = (ImageButton) findViewById(R.id.ibUploadPic);
+        ibUploadPic.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 MarkerDetailsActivityPermissionsDispatcher.onLaunchCameraWithCheck(MarkerDetailsActivity.this, v);
@@ -66,7 +74,6 @@ public class MarkerDetailsActivity extends AppCompatActivity {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         // why can't it get the file name?
         intent.putExtra(MediaStore.EXTRA_OUTPUT, getPhotoFileUri(photoFileName)); // set the image file name
-
         // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
         // So as long as the result is not null, it's safe to use the intent.
         if (intent.resolveActivity(getPackageManager()) != null) {
@@ -87,7 +94,7 @@ public class MarkerDetailsActivity extends AppCompatActivity {
             if (resultCode == RESULT_OK) {
                 Uri takenPhotoUri = getPhotoFileUri(photoFileName);
                 // by this point we have the camera photo on disk
-                Bitmap takenImage = BitmapFactory.decodeFile("/storage/emulated/0/Android/data/com.example.mapdemo/files/Pictures/SeenAds/photo.jpg");
+                Bitmap takenImage = BitmapFactory.decodeFile(ABSOLUTE_FILE_PATH + photoFileName);
                 // RESIZE BITMAP, see section below
                 // Load the taken image into a preview
                 ivMarkerPhoto.setImageBitmap(takenImage);
@@ -106,15 +113,12 @@ public class MarkerDetailsActivity extends AppCompatActivity {
             // This way, we don't need to request external read/write runtime permissions.
             File mediaStorageDir = new File(
                     getExternalFilesDir(Environment.DIRECTORY_PICTURES), APP_TAG);
-
             // Create the storage directory if it does not exist
             if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
                 Log.d(APP_TAG, "failed to create directory");
             }
-
             // Return the file target for the photo based on filename
             File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
-
             // wrap File object into a content provider
             // required for API >= 24
             // See https://guides.codepath.com/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
