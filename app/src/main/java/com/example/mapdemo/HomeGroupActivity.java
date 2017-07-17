85401d0ad51700c371d52ee3d6a0480e6dc4686d
Taylor Jean-Jacques <tjeanjacques@fb.com>
2017-07-17 10:51:13 -0700

    setup parse class

--- app/src/main/java/com/example/mapdemo/HomeGroupActivity.java
+++ app/src/main/java/com/example/mapdemo/HomeGroupActivity.java
@@ -1,30 +1,4 @@
 package com.example.mapdemo;
 
-import android.app.Application;
-
-import com.parse.Parse;
-
-import okhttp3.OkHttpClient;
-import okhttp3.logging.HttpLoggingInterceptor;
-
-public class HomeGroupActivity extends Application {
-    @Override
-    public void onCreate() {
-        super.onCreate();
-
-        // Use for monitoring Parse network traffic
-        OkHttpClient.Builder builder = new OkHttpClient.Builder();
-        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
-        // Can be Level.BASIC, Level.HEADERS, or Level.BODY
-        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
-        builder.networkInterceptors().add(httpLoggingInterceptor);
-
-        // set applicationId and server based on the values in the Heroku settings.
-        // any network interceptors must be added with the Configuration Builder given this syntax
-        Parse.initialize(new Parse.Configuration.Builder(this)
-                .applicationId("YOUR_APPLICATION_ID") // should correspond to APP_ID env variable
-                .clientBuilder(builder)
-                .server("https://myparseapp.herokuapp.com/parse/").build());
-    }
-}
+public class HomeGroupActivity {}
 
